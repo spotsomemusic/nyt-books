25f155207b7a38f44a58bb31e3b59f9de9a16cbb
Twincasper <justinwatson651@gmail.com>
2024-03-19 16:16:11 -0400

    Created component for CurrentBestSellers page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,46 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { handleFetch } from './utils/fetchData';
+import React from 'react';
+import CurrentBestSellers from './Component/CurrentBestSellers';
 import './App.css';
 
-const NYT_API_KEY = 'ZzgeKyhP0Ly4wfA7p8cK2VQlzgbDQQO3';
-
 function App() {
-  const [booksData, setBooksData] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-        const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${NYT_API_KEY}`;
-        const [data, error] = await handleFetch(url);
-
-        if (data) setBooksData(data); 
-        if (error) setError(error);
-      }
-    fetchData();
-  }, []);
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
   return (
     <div className="App">
-      <h1>New York Times Best Sellers</h1>
-      {booksData && (
-        <ul>
-          <li>
-            <h2>{booksData.results.display_name}</h2>
-            <ul>
-              {booksData.results.books.map((book, index) => (
-                <li key={index}>
-                  <h3>{book.title}</h3>
-                  <p>{book.author}</p>
-                </li>
-              ))}
-            </ul>
-          </li>
-        </ul>
-      )}
+      <CurrentBestSellers />
     </div>
   );
 }
---
+++ app/src/Component/CurrentBestSellers.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { handleFetch } from '../utils/fetchData';
+
+const NYT_API_KEY = 'ZzgeKyhP0Ly4wfA7p8cK2VQlzgbDQQO3';
+
+function CurrentBestSellers() {
+  const [booksData, setBooksData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${NYT_API_KEY}`;
+        const [data, error] = await handleFetch(url);
+
+        if (data) setBooksData(data); 
+        if (error) setError(error);
+      }
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>New York Times Best Sellers</h1>
+      {booksData && (
+        <ul>
+          <li>
+            <h2>{booksData.results.display_name}</h2>
+            <ul>
+              {booksData.results.books.map((book, index) => (
+                <li key={index}>
+                  <h3>{book.title}</h3>
+                  <p>{book.author}</p>
+                  <img src={book.book_image} alt={book.title}></img>
+                </li>
+              ))}
+            </ul>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default CurrentBestSellers;
