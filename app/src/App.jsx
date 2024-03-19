38869b4f455a1074de56aac0d3e3f22ea780a4c1
Twincasper <justinwatson651@gmail.com>
2024-03-19 16:27:14 -0400

    Reformated app.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import CurrentBestSellers from './Component/CurrentBestSellers';
 import './App.css';
 
+
+export default App;
+
 function App() {
   return (
+    <>
+      <header>
+        {/* nav will go here*/}
+      </header>
+      <main>
     <div className="App">
       <CurrentBestSellers />
     </div>
-  );
+      </main>
+      <footer>
+        {/* dev information will go here*/}
+      </footer>
+    </>
+  )
 }
-
-export default App;
