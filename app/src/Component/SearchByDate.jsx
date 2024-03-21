f3906492eb5149dd464e51b685fbffe769d757fb
Twincasper <justinwatson651@gmail.com>
2024-03-21 10:23:57 -0400

    Fixed css for date picker

--- app/src/Component/SearchByDate.jsx
+++ app/src/Component/SearchByDate.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
-//import "react-datepicker/dist/react-datepicker.css";
+import "react-datepicker/dist/react-datepicker.css";
 import { handleFetch } from "../utils/fetchData";
 import PropTypes from "prop-types";
 
